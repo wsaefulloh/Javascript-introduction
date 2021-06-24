95760a89601382d8ab75096f19bb78622884a50d
Wahyu Saefulloh <saefuuloh@gmail.com>
2021-06-24 12:54:18 +0700

    menyelesaikan tugas 5 dan 6

---
+++ tugas3_nomor5.js
@@ -0,0 +1,9 @@
+const fetch = require('node-fetch');
+fetch('https://jsonplaceholder.typicode.com/users')
+.then(async (response) => {
+    let data = await response.json()
+    console.log(data.map((data) => {return data.name}))
+})
+
+//.then(res => res.json())
+    //.then(json => console.log(json.map(json => json.name)))
