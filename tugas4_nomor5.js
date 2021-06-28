fdd4b1c8f14d156b0893b0b6ef92ec52ce4835d6
Wahyu Saefulloh <saefuuloh@gmail.com>
2021-06-28 16:00:39 +0700

    menambahkan gambar flowchart dan algoritma

--- tugas4_nomor5.js
+++ tugas4_nomor5.js
@@ -1,18 +1,24 @@
-const divideAndSort = ((num) => {
-    let newString = num.toString()
-    let newArray = newString.split('0')
-    let fixArray = []
+const divideAndSort = (data) => {
+  if (typeof(data) == 'number') {
+    let newString = data.toString();
+    let newArray = newString.split("0");
+    let fixArray = [];
     for (let i = 0; i < newArray.length; i++) {
-        let newSortArr = newArray[i].split('')
-        let cek = newSortArr.sort()
-        fixArray.push(cek)
+      let newSortArr = newArray[i].split("");
+      let cek = newSortArr.sort();
+      fixArray.push(cek);
     }
-    let fixSort = []
+    let fixSort = [];
     for (let i = 0; i < fixArray.length; i++) {
-        let Sort = fixArray[i].join('')
-        fixSort.push(Sort)
+      let Sort = fixArray[i].join("");
+      fixSort.push(Sort);
     }
-    console.log(fixSort.join(''))
-})
+    let fix = fixSort.join("")
+    fix = parseInt(fix)
+    console.log(fix)
+  } else {
+    console.log("data yang dimasukkan harus berupa number");
+  }
+};
 
-divideAndSort(5956560159466056)
+divideAndSort(5956560159466056);
