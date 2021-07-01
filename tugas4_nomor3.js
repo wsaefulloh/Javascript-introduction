712e0ae4f1366cd2f8157367fb841c56d0e8baa8
Wahyu Saefulloh <saefuuloh@gmail.com>
2021-07-01 18:56:17 +0700

    update tugas 4 nomor 3

--- tugas4_nomor3.js
+++ tugas4_nomor3.js
@@ -1,45 +1,61 @@
 const name = [
-    'Abigail', 'Alexandra', 'Fatih',
-    'Amanda', 'Angela', 'Bella',
-    'Carol', 'Caroline', 'fatih',
-    'Deirdre', 'Diana', 'Elizabeth',
-    'Ella', 'Faith', 'Olivia', 'Penelope',
-    'Diani', 'fatih', 'carol' , 'fatih', 'Faith'
-]
+    "Abigail",
+    "Alexandra",
+    "Fatih",
+    "Amanda",
+    "Angela",
+    "Bella",
+    "Carol",
+    "Caroline",
+    "fatih",
+    "Deirdre",
+    "Diana",
+    "Elizabeth",
+    "Ella",
+    "Faith",
+    "Olivia",
+    "Penelope",
+    "Diani",
+    "fatih",
+    "carol",
+    "fatih",
+    "Faith",
+];
 
-const findRepeatName = ((data) => {
+const findRepeatName = (data) => {
     if (data != null) {
-        let cek = -1
-        let freq = []
+        let cek = -1;
+        let freq = [];
         for (let i = 0; i < data.length; i++) {
             for (let j = 0; j < data.length; j++) {
                 if (data[i].toLowerCase() == data[j].toLowerCase()) {
-                    cek = cek + 1
+                    cek = cek + 1;
                 } else {
-                    cek = cek + 0
+                    cek = cek + 0;
                 }
             }
-            freq.push(cek)
-            cek = -1
+            freq.push(cek);
+            cek = -1;
         }
-        console.log(data)
-        console.log(freq)
+        console.log(data);
+        console.log(freq);
 
-        let sorted = freq.sort((a,b)=>{return a-b})
+        let sorted = freq.sort((a, b) => {
+            return a - b;
+        });
 
-        console.log(sorted)
-        console.log(sorted[sorted.length - 1])
+        console.log(sorted);
+        console.log(sorted[sorted.length - 1]);
 
         for (let i = 0; i < sorted.length; i++) {
             if (freq[i] == sorted[sorted.length - 1]) {
-                console.log(data[i])
-                break
+                console.log(data[i]);
+                break;
             }
         }
-
     } else {
-        console.log('Data tidak boleh kosong')
+        console.log("Data tidak boleh kosong");
     }
-})
+};
 
-findRepeatName(name)
+findRepeatName(name);
